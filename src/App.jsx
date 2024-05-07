43543463b39886fa6985b7458a3e52ee475c3a47
Angelo//Telamo <126788602+AngeloTelamo@users.noreply.github.com>
2024-05-07 10:15:44 +0800

    homepage problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import ButtonGradient from './assets/svg/ButtonGradient';
 import './App.css'
 import Button from './Components/Button';
 import Header from './Components/Header';
+import Hero from './Components/Hero';
 
 const App = () => {
   return (
   <>
         <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
            <Header/>
+           <Hero/>
         </div>
           <ButtonGradient/>
     </>
